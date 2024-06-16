39a685c653bc21349d77945194ea5424c5155486
MaestroFidel <joyspider559@gmail.com>
2024-06-16 14:43:24 +0300

    hw-js-2

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function checkForSpam(message) {
+  const alarmWord1 = "spam";
+  const alarmWord2 = "sale";
+
+  return (
+    message.toLowerCase().includes(alarmWord1) ||
+    message.toLowerCase().includes(alarmWord2)
+  );
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
